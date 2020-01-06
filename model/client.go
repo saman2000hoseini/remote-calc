2d79a0a3107651c41fcaaeda52f34bb273dac907
saman2000hoseini <46269505+saman2000hoseini@users.noreply.github.com>
2020-01-06 04:57:46 +0330

    client's bug fixed

--- model/client.go
+++ model/client.go
@@ -11,43 +11,12 @@ type Client struct {
 	connection net.Conn
 	writer     *bufio.Writer
 	reader     *bufio.Reader
-	req        chan string
-	connected  chan bool
 }
 
 func NewClient(connection net.Conn) *Client {
 	return &Client{connection: connection, writer: bufio.NewWriter(connection), reader: bufio.NewReader(connection)}
 }
 
-/*
-func (c *Client) startClientHandler() {
-	for {
-		select {
-		case req := <-c.req:
-			op, err := NewOperation(req)
-			if err != nil {
-				panic(err)
-				break
-			}
-			er := op.Calculate()
-			if er != nil {
-				panic(er)
-				break
-			}
-			response, _ := json.Marshal(op.Result)
-			fmt.Println(op.Result)
-			c.writer.WriteString(string(response) + "\n")
-			c.writer.Flush()
-		case connected := <-c.connected:
-			if !connected {
-				c.connection.Close()
-				return
-			}
-
-		}
-	}
-}
-*/
 func (c *Client) StartResponseHandler() {
 	for {
 		res, err := c.reader.ReadString('\n')
@@ -64,12 +33,12 @@ func (c *Client) Request(req string) {
 }
 
 func (c *Client) Listen() {
-	//	go c.startClientHandler()
 	for {
 		req, err := c.reader.ReadString('\n')
 		if err != nil {
 			panic(err)
-			c.connected <- false
+			c.connection.Close()
+                        return
 		} else {
 			op, err := NewOperation(req)
 			if err != nil {
@@ -85,8 +54,6 @@ func (c *Client) Listen() {
 			fmt.Println(op.Result)
 			c.writer.WriteString(string(response) + "\n")
 			c.writer.Flush()
-
-			//c.req <- req
 		}
 	}
 }
